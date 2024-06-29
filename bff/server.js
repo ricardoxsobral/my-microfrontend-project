2aa279b88efb4b90765af93a5d20971a2ccd60fb
Ricardo Sobral <ricardoxsobral@gmail.com>
2024-06-29 11:07:59 -0300

    [feat] Adding CORS to BFF

--- bff/server.js
+++ bff/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 
 const apiKey = 'AIzaSyD1kiPGSh0dga__S9rZ3DDJcJGWP05VU8o';
+
+app.use(cors());
 
 app.get('/search', async (req, res) => {
   const { query } = req.query;
   try {
     const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${apiKey}`);
-    console.log('API Response:', response.data);
     res.json(response.data);
   } catch (error) {
-    console.error('API Error:', error.message);
     res.status(500).send(error.message);
   }
 });
