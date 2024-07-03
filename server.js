e9c2808e2b9abe85f6d394f331b210535698847f
Ricardo Sobral <ricardoxsobral@gmail.com>
2024-07-03 15:52:05 -0300

    [feat] Adding index.html to port in localhost

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3004;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
